88e8dadebcc7dc58fb4a998e47f3ebf98fde5fbc
Ibraheem <ialqwaiz.sta@gmail.com>
2026-04-22 12:16:31 +0300

    old code removed

--- border.sql
+++ border.sql
@@ -1,49 +1,6 @@
-
-
-IF OBJECT_ID('tempdb..#border') IS NOT NULL DROP TABLE #border;
-with border as ( -- MT.tdp.BOARDER_INBOUND_VW
-select
-    YEAR(TRAVEL_DATE) as year,
-    MONTH(TRAVEL_DATE) as month,
-    DAY(TRAVEL_DATE) as day,
---    FROM_COUNTRY_NAME_EN as country,
-    NATIONALITY_COUNTRY_EN as country,
-    sum(NUMBER_OF_TOURIST) as border_daily_visits,
-    
-    sum(sum(NUMBER_OF_TOURIST)) OVER (
-        partition by 
-            YEAR(TRAVEL_DATE),
-            MONTH(TRAVEL_DATE),
-            --FROM_COUNTRY_NAME_EN
-            NATIONALITY_COUNTRY_EN
-    ) as border_monthly_visits,
-    
-    -- daily percentage of monthly total
-    (1.0 * sum(NUMBER_OF_TOURIST)) / sum(sum(NUMBER_OF_TOURIST)) OVER (
-    partition by 
-                YEAR(TRAVEL_DATE),
-                MONTH(TRAVEL_DATE),
-                --FROM_COUNTRY_NAME_EN
-                NATIONALITY_COUNTRY_EN
-        ) as border_daily_ratio
-
-from MT.tdp.BOARDER_INBOUND_VW
--- where year(TRAVEL_DATE) >= @start_year
-group by
-    YEAR(TRAVEL_DATE),
-    MONTH(TRAVEL_DATE),
-    DAY(TRAVEL_DATE),
-    --FROM_COUNTRY_NAME_EN
-    NATIONALITY_COUNTRY_EN
-) select * into #border from border 
-
-select * from #border
-
-
------------
-
-
-
+------------------------------------------------------------
+-- daily_inbound_estimates
+------------------------------------------------------------
 
 
 IF OBJECT_ID('tempdb..#border') IS NOT NULL DROP TABLE #border;
--- targets.sql
+++ targets.sql
@@ -1,15 +1,22 @@
-select
-	    vt.Year as year,
-	    vt.MonthNo as month,
-	    vt.Country as country,
-	    vt.BU,
-	    vt.Purpose as purpose,
-	    vt.Committed_value as visits_target,
-	    st.Committed_value as spend_target
-	FROM [SIDR].[Business_Review].[Fact_Visits_Target] vt
-	JOIN [SIDR].[Business_Review].[Fact_Spend_Target] st
-	      ON  st.Year      = vt.Year
-	      AND st.MonthNo   = vt.MonthNo
-	      AND st.Country   = vt.Country
-	      AND st.BU        = vt.BU
-	      AND st.Purpose   = vt.Purpose
+------------------------------------------------------------
+-- targets
+------------------------------------------------------------
+
+IF OBJECT_ID('tempdb..#targets') IS NOT NULL DROP TABLE #targets;
+with targets AS (
+    SELECT
+        vt.Year AS year,
+        vt.MonthNo AS month,
+        vt.Country AS country,
+        vt.BU,
+        vt.Purpose AS purpose,
+        vt.Committed_value AS visits_target,
+        st.Committed_value AS spend_target
+    FROM [SIDR].[Business_Review].[Fact_Visits_Target] vt
+    JOIN [SIDR].[Business_Review].[Fact_Spend_Target] st
+          ON  st.Year    = vt.Year
+          AND st.MonthNo = vt.MonthNo
+          AND st.Country = vt.Country
+          AND st.BU      = vt.BU
+          AND st.Purpose = vt.Purpose
+)SELECT * INTO #targets FROM targets;
