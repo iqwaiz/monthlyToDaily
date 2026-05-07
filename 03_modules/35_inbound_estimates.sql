3b5d6268c8e259d39f45bd4fbf750967f9990d37
Ibraheem <ialqwaiz.sta@gmail.com>
2026-05-07 08:41:17 +0300

    added another source (TOURISM_FLOWS_YTD_TBL) for inbound_estimates

--- 03_modules/35_inbound_estimates.sql
+++ 03_modules/35_inbound_estimates.sql
@@ -14,110 +14,85 @@ BEGIN
 
 
 
+DECLARE @inbound_estimates_start date;
 
-DECLARE @inbound_estimates_start date;-- = (SELECT MAX(DATEFROMPARTS(YEAR, [MONTH_NUM],1)) FROM [MT].[mas].[MAS_INBOUND_INC_VW]);
-
---SELECT @inbound_estimates_start = MAX(DATEFROMPARTS(YEAR, [MONTH_NUM],1)) FROM [MT].[mas].[MAS_INBOUND_INC_VW]  WITH (NOLOCK);
 set @inbound_estimates_start = '2024-01-01';
 
 DECLARE @T SYSNAME = 'daily_inbound_estimates';
 IF OBJECT_ID('tempdb..#result') IS NOT NULL DROP TABLE #result;
 
-with inbound_visit_estimates as(
+with monthly_inbound_estimates as(
 	SELECT
-		est.date_estimate,
-		--r.Busines_Unit,
-		est.origin_country,
-        est.purpose,
-		SUM(est.border_visits_estimate_coeffeicient) as visits,
-        SUM(est.alos * est.border_visits_estimate_coeffeicient) as nights
-	FROM [ANALYTICS].[dbo].[INBOUND_VISITS_ESTIMATION] as est with(nolock)
-	--LEFT JOIN SIDR.dbo.Ref_Country as c
-	--ON est.origin_country = c.Country_Name_En
-	--LEFT JOIN [SIDR].[dbo].[Rel_BU_level_Country] as r with(nolock)
-	--ON r.Country_Key = c.country_key
-	WHERE est.date_estimate >= @inbound_estimates_start
-	AND est.purpose <> 'Hajj'
-	GROUP BY
-		est.date_estimate,
-		--r.Busines_Unit,
-		est.origin_country,
-		est.purpose
-),
-inbound_spend_estimates as (
-	SELECT 
-		est.date_estimate,
-		--r.Busines_Unit,
-		est.origin_country,
-        est.purpose,
-		SUM(est.spend_estimate_coeffeicient) as spend
-	FROM [ANALYTICS].[dbo].[INBOUND_SPEND_ESTIMATION] as est with(nolock)
-	--LEFT JOIN SIDR.dbo.Ref_Country as c
-	--ON est.origin_country = c.Country_Name_En
-	--LEFT JOIN [SIDR].[dbo].[Rel_BU_level_Country] as r with(nolock)
-	--ON r.Country_Key = c.country_key
-	WHERE est.date_estimate >= @inbound_estimates_start
-	AND est.purpose <> 'Hajj'
-	GROUP BY
-		est.date_estimate,
-		--r.Busines_Unit,
-		est.origin_country,
-		est.purpose
-
-),
-monthly_inbound_estimates as(
-	select
-		'inbound_estimate' as data_type,
-	    v.date_estimate as date,
-	    YEAR(v.date_estimate) as year,
+		'inbound_analytics_estimates' as data_type,
+		v.date_estimate,
+		YEAR(v.date_estimate) as year,
 	    MONTH(v.date_estimate) as month,
-	    -- day(EOMONTH(DATEFROMPARTS(year(v.date_estimate), month(v.date_estimate), 1))) as days_in_month,
 	    DAY(EOMONTH(v.date_estimate)) as days_in_month,
-	    v.purpose,
-	    v.origin_country as country,
-	    -- b.Busines_Unit as BU,
-	    v.visits,
-	    s.spend,
-	    v.nights
-        -- t.visits_target,
-        -- t.spend_target
-	FROM inbound_visit_estimates v
-	LEFT JOIN inbound_spend_estimates s
-	    ON v.date_estimate = s.date_estimate
+		v.origin_country  as country,
+        v.purpose,
+		SUM(v.border_visits_estimate_coeffeicient) as visits,
+        SUM(v.alos * v.border_visits_estimate_coeffeicient) as nights,
+        SUM(s.spend_estimate_coeffeicient) as spend
+	FROM [ANALYTICS].[dbo].[INBOUND_VISITS_ESTIMATION] as v with(nolock)
+	LEFT JOIN [ANALYTICS].[dbo].[INBOUND_SPEND_ESTIMATION] as s with(nolock)
+		ON v.date_estimate = s.date_estimate
 	   AND v.origin_country = s.origin_country
 	   AND v.purpose = s.purpose
-	LEFT JOIN SIDR.dbo.Ref_Country c
-	    ON v.origin_country = c.Country_Name_En
-	-- LEFT JOIN SIDR.dbo.Rel_BU_level_Country b WITH (NOLOCK)
-	--     ON b.Country_Key = c.country_key
-    -- LEFT JOIN ibraheem_test.dailyData.targets t
-	-- 	ON t.year    = YEAR(v.date_estimate)
-	-- 	AND t.month   = MONTH(v.date_estimate)
-	-- 	AND t.country = v.origin_country
-	-- 	AND t.BU      = b.Busines_Unit
-	-- 	AND t.purpose = v.purpose
-), daily_inbound_estimates as(
+	WHERE v.date_estimate >= @inbound_estimates_start
+	AND v.purpose <> 'Hajj'
+	GROUP BY
+		v.date_estimate,
+		v.origin_country,
+		v.purpose
+), daily_inbound_estimates as (
 	select
 	    m.data_type,
---	    m.date,
---	    DATEADD(DAY, d.day - 1, m.date) as full_date,
 	    DATEFROMPARTS(m.year, m.month, d.[DayofMonth]) as date,
 	    m.year,
 	    m.month,
 	    d.[DayofMonth] as day,
---	    m.days_in_month,
 	    m.purpose,
 	    m.country,
-	    -- m.BU,
 	    m.visits / m.days_in_month as daily_visits,
 	    m.spend  / m.days_in_month as daily_spend,
 	    m.nights / m.days_in_month as daily_nights
-	    -- m.visits_target / m.days_in_month as visits_target,
-        -- m.spend_target  / m.days_in_month as spend_target
 	FROM monthly_inbound_estimates m
 	join SIDR.dbo.DIM_DATE d
 	    on d.[YEAR] = m.[year] and d.[MONTH] = m.month
-)SELECT * INTO #result FROM daily_inbound_estimates;
+), flows_estimates as (
+	select 
+		'inbound_flows_estimates' as data_type,
+		d.DateFormat1 as date,
+		d.[YEAR] as year,
+		d.[MONTH] as month,
+		d.DayofMonth as day,
+		f.[FROM_COUNTRY] as country,
+		f.PURPOSE as purpose,
+		f.Visitors_YTD as daily_visits,
+		f.Spend_YTD as daily_spend
+	from [MT].[estimates].[TOURISM_FLOWS_YTD_TBL] f
+	left join SIDR.dbo.DIM_DATE d
+		on f.DATE_KEY = d.ID_Day	
+	where
+		YTD_Source = 'Estimated'
+		and TOURIST_TYPE = 'Inbound'
+), combined as (
+	SELECT 
+		COALESCE(t1.data_type, t2.data_type) AS data_type,
+	    COALESCE(t1.date, t2.date) AS date,
+	    COALESCE(t1.year, t2.year) AS year,
+	    COALESCE(t1.month, t2.month) AS month,
+	    COALESCE(t1.day, t2.day) AS day,
+	    COALESCE(t1.country, t2.country) AS country,
+	    COALESCE(t1.purpose, t2.purpose) AS purpose,
+	    COALESCE(t1.daily_visits, t2.daily_visits) AS daily_visits,
+	    COALESCE(t1.daily_spend, t2.daily_spend) AS daily_spend
+	FROM daily_inbound_estimates t1
+	FULL OUTER JOIN flows_estimates t2
+	    ON  t1.date = t2.date
+	    AND t1.country = t2.country 
+	    AND t1.purpose = t2.purpose
+)SELECT * INTO #result FROM combined;
 
 
 EXEC ibraheem_test.dailyData.usp_UpsertDailyTable @T;
